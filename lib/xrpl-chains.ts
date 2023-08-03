83ac55d2321f9a5b852d714e8b9e37818e0e0ff2
CryptoCraftsman-Web3 <taylormoore97521@gmail.com>
2023-08-03 09:52:55 +0800

    Create xrpl-chains.ts
    Add XRPL Devnet custom chain

---
+++ lib/xrpl-chains.ts
@@ -0,0 +1,20 @@
+import { Chain } from 'wagmi'
+
+export const xrplDevnet = {
+  id: 1440002,
+  name: 'XRPL Devnet',
+  network: 'xrpl-devnet',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'XRP',
+    symbol: 'XRP',
+  },
+  rpcUrls: {
+    public: { http: ['https://rpc-evm-sidechain.xrpl.org'] },
+    default: { http: ['https://rpc-evm-sidechain.xrpl.org'] },
+  },
+  blockExplorers: {
+    etherscan: { name: 'XRP Ledger', url: 'https://evm-sidechain.xrpl.org' },
+    default: { name: 'XRP Ledger', url: 'https://evm-sidechain.xrpl.org' },
+  },
+} as const satisfies Chain
