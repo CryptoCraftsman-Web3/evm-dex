9a16bf6c91d8be007110754ab28d30d26050a712
CryptoCraftsman-Web3 <taylormoore97521@gmail.com>
2023-08-04 10:13:02 +0800

    Add feeAmounts to config

--- components/config.ts
+++ components/config.ts
@@ -17,4 +17,21 @@ export const config = {
       href: '/pools',
     }
   ],
+  feeAmounts: [
+    {
+      label: '0.05%',
+      value: 500,
+      tip: 'Best for stable pairs'
+    },
+    {
+      label: '0.3%',
+      value: 3000,
+      tip: 'Best for most pairs'
+    },
+    {
+      label: '1%',
+      value: 10000,
+      tip: 'Best for exotic pairs'
+    }
+  ]
 };
