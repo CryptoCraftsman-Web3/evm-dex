a30c94ac794f9b2fa5d3a9caf5dbaba1fbbe303b
CryptoCraftsman-Web3 <taylormoore97521@gmail.com>
2023-08-14 10:18:57 +0800

    Add and test uniswaps deployment

--- hooks/swap-protocol-hooks.ts
+++ hooks/swap-protocol-hooks.ts
@@ -12,7 +12,8 @@ export const usePoolFactory = (): `0x${string}` => {
       break;
     case 11155111:
       // Sepolia Testnet
-      poolFactoryAddress = '0x76DDF5E3Cb4412fD8dc39440c56c0C58D0705290';
+      // poolFactoryAddress = '0x76DDF5E3Cb4412fD8dc39440c56c0C58D0705290'; our deployed pool factory
+      poolFactoryAddress = '0x1F98431c8aD98523631AE4a59f267346ea31F984'; // uniswap deployed pool factory
       break;
     default:
       poolFactoryAddress = '0x0000000000000000000000000000000000000000';
