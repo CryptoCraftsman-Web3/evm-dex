3bc4f4305a84d68628470fc34054d83b6324c325
CryptoCraftsman-Web3 <taylormoore97521@gmail.com>
2023-08-14 10:51:01 +0800

    Set validPriceRange prop

--- components/pools/new-liquidity-position.tsx
+++ components/pools/new-liquidity-position.tsx
@@ -148,6 +148,7 @@ const NewLiquidityPosition = () => {
                   setAmountA={setAmountA}
                   amountB={amountB}
                   setAmountB={setAmountB}
+                  validPriceRange={validPriceRange}
                 />
               )}
             </Stack>
