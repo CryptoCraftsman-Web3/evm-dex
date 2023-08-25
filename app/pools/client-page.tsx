0b36a4e6d7d9cffdb70e36b324009cf5fe879bcd
CryptoCraftsman-Web3 <taylormoore97521@gmail.com>
2023-08-25 10:42:48 +0800

    Refactor layout for reuse

--- app/pools/client-page.tsx
+++ app/pools/client-page.tsx
@@ -29,65 +29,53 @@ const PoolsClientPage = () => {
   });
 
   return (
-    <Box
-      px={{ xs: 2, md: 6 }}
-      pt={{ xs: 4, md: 8 }}
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-    >
+    <>
       <Stack
-        direction="column"
-        spacing={4}
-        width={{ xs: '100%', md: '600px', lg: '800px' }}
+        direction="row"
+        justifyContent="space-between"
+        width="100%"
       >
-        <Stack
-          direction="row"
-          justifyContent="space-between"
-          width="100%"
-        >
-          <Typography variant="h4">
-            <b>Pools</b>
-          </Typography>
+        <Typography variant="h4">
+          <b>Pools</b>
+        </Typography>
 
-          <NewLiquidityPosition refetchPoolsCount={refetchPoolsCount} />
-        </Stack>
-
-        <Paper
-          variant="outlined"
-          sx={{
-            p: { xs: 2, md: 4 },
-            w: { xs: '100%', md: '600px', lg: '800px' }
-          }}
-        >
-          {isConnected ? (
-            <PoolsList
-              poolsCount={poolsCount || 0n}
-              isGettingPoolsCount={isGettingPoolsCount}
-              refetchPoolsCount={refetchPoolsCount}
-            />
-          ) : (
-            <Stack
-              direction="column"
-              spacing={2}
-              justifyContent="center"
-              alignItems="center"
-            >
-              <IoFileTrayStackedOutline size={96} />
-
-              <Typography variant="body1">Your liquidity positions will appear here</Typography>
-
-              <Button
-                variant="outlined"
-                onClick={() => setConnectWalletOpen(true)}
-              >
-                Connect Wallet
-              </Button>
-            </Stack>
-          )}
-        </Paper>
+        <NewLiquidityPosition refetchPoolsCount={refetchPoolsCount} />
       </Stack>
-    </Box>
+
+      <Paper
+        variant="outlined"
+        sx={{
+          p: { xs: 2, md: 4 },
+          w: { xs: '100%', md: '600px', lg: '800px' },
+        }}
+      >
+        {isConnected ? (
+          <PoolsList
+            poolsCount={poolsCount || 0n}
+            isGettingPoolsCount={isGettingPoolsCount}
+            refetchPoolsCount={refetchPoolsCount}
+          />
+        ) : (
+          <Stack
+            direction="column"
+            spacing={2}
+            justifyContent="center"
+            alignItems="center"
+          >
+            <IoFileTrayStackedOutline size={96} />
+
+            <Typography variant="body1">Your liquidity positions will appear here</Typography>
+
+            <Button
+              variant="outlined"
+              onClick={() => setConnectWalletOpen(true)}
+            >
+              Connect Wallet
+            </Button>
+          </Stack>
+        )}
+      </Paper>
+    </>
   );
 };
 
--- components/main-content.tsx
+++ components/main-content.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Box } from '@mui/material';
+import { Box, Stack } from '@mui/material';
 
 type MainContentProps = {
   children: React.ReactNode;
@@ -9,9 +9,23 @@ type MainContentProps = {
 const MainContent = ({ children }: MainContentProps) => {
   return (
     <Box sx={{ my: 8 }}>
-      {children}
+      <Box
+        px={{ xs: 2, md: 6 }}
+        pt={{ xs: 4, md: 8 }}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Stack
+          direction="column"
+          spacing={4}
+          width={{ xs: '100%', md: '600px', lg: '800px' }}
+        >
+          {children}
+        </Stack>
+      </Box>
     </Box>
-  )
+  );
 };
 
 export default MainContent;
