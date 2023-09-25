1332159ab94e7de08a18a2c788af8fbdbd8211c8
CryptoCraftsman-Web3 <taylormoore97521@gmail.com>
2023-09-25 14:01:32 +0800

    EVMDEX-1 added quoterV2 addresses

--- hooks/swap-protocol-hooks.ts
+++ hooks/swap-protocol-hooks.ts
@@ -5,6 +5,7 @@ type SwapProtocolAddresses = {
   poolFactory: `0x${string}`;
   nfPositionManager: `0x${string}`;
   serpentSwapUtility: `0x${string}`;
+  quoterV2: `0x${string}`;
 };
 
 export const useSwapProtocolAddresses = (): SwapProtocolAddresses => {
@@ -13,6 +14,7 @@ export const useSwapProtocolAddresses = (): SwapProtocolAddresses => {
   let poolFactory: `0x${string}` = zeroAddress;
   let nfPositionManager: `0x${string}` = zeroAddress;
   let serpentSwapUtility: `0x${string}` = zeroAddress;
+  let quoterV2: `0x${string}` = zeroAddress;
 
   switch (chain?.id) {
     case 1440002:
@@ -20,6 +22,7 @@ export const useSwapProtocolAddresses = (): SwapProtocolAddresses => {
       poolFactory = '0x71992849909a5Ed0c8Cc3928F5F5287B13d08aBA';
       nfPositionManager = '0xd70B1Ac076ad3B7155735E3CfC8ED24DF37D3235';
       serpentSwapUtility = '0x1f9dE68B808a758D5eB56EB10e62d6360B79d210';
+      quoterV2 = '0x2abc3CFB89DE3685e10C61f0E763131D63aC14a6';
       break;
     case 11155111:
       // Sepolia Testnet (our own deployments)
@@ -30,10 +33,13 @@ export const useSwapProtocolAddresses = (): SwapProtocolAddresses => {
       poolFactory = '0x0227628f3F023bb0B980b67D528571c95c6DaC1c';
       nfPositionManager = '0x1238536071E1c677A632429e3655c799b22cDA52';
       serpentSwapUtility = '0x6C45c98B989058feF7e6d0701515Ad6238ac45C6';
+      quoterV2 = '0xd96541ee98E931a712B3c2BE24f675E67F62E102';
       break;
     default:
       poolFactory = zeroAddress;
       nfPositionManager = zeroAddress;
+      serpentSwapUtility = zeroAddress;
+      quoterV2 = zeroAddress;
       break;
   }
 
@@ -41,5 +47,6 @@ export const useSwapProtocolAddresses = (): SwapProtocolAddresses => {
     poolFactory,
     nfPositionManager,
     serpentSwapUtility,
+    quoterV2,
   };
 };
