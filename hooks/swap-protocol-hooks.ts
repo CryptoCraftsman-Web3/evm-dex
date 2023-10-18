4d2568dfdc179183f84c32b85d4ec7a0a02e33b2
CryptoCraftsman-Web3 <taylormoore97521@gmail.com>
2023-10-18 13:17:53 +0800

    EVMDEX-20 replace swap router addresses
    Update

--- hooks/swap-protocol-hooks.ts
+++ hooks/swap-protocol-hooks.ts
@@ -25,7 +25,7 @@ export const useSwapProtocolAddresses = (): SwapProtocolAddresses => {
       nfPositionManager = '0x2Bd64c4C3C59b2f4B8f5A077c17eD9F9e816dF64';
       serpentSwapUtility = '0x1f9dE68B808a758D5eB56EB10e62d6360B79d210';
       quoterV2 = '0xDb3721EBB2CC650e4A41f685294b51406F6Ab5D4';
-      swapRouter = '0xD332359cd2AB1198ECfE355852d3bDE6EA59BeE2';
+      swapRouter = '0x8E839AA95Ab9EeA254373a722159D96D7DedAfaB';
       break;
     case 11155111:
       // Sepolia Testnet (our own deployments)
@@ -37,7 +37,7 @@ export const useSwapProtocolAddresses = (): SwapProtocolAddresses => {
       nfPositionManager = '0xd33dde75E3105e3506b437438fedDC27a2Dd7A95';
       serpentSwapUtility = '0x6C45c98B989058feF7e6d0701515Ad6238ac45C6';
       quoterV2 = '0x1BF0070245CF5C9Ed97B6b41E8a087703fd76e1B';
-      swapRouter = '0x2Bd2Fa75C8D18fD0a7728E7fB196aE0C4f5B6d51';
+      swapRouter = '0x8049c9E3cE496b47E0fE8aa8EdAEf751cF87e07d';
       break;
     default:
       poolFactory = zeroAddress;
