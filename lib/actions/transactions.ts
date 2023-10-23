bb54f133ae827938547a2b458b8a4c3fb99c6caf
CryptoCraftsman-Web3 <taylormoore97521@gmail.com>
2023-10-23 15:22:51 +0800

    EVMDEX-25 create server actions for user transactions

---
+++ lib/actions/transactions.ts
@@ -0,0 +1,7 @@
+'use server';
+
+import { Transaction } from '../db-schemas/transaction';
+
+const saveTransaction = async (transaction: Transaction) => {
+  // verify if the transaction exists in blockchain
+};
