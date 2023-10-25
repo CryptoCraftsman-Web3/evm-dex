91158a6df53b030a078b141eb98e7ae1c31cb409
CryptoCraftsman-Web3 <taylormoore97521@gmail.com>
2023-10-25 14:52:12 +0800

    EVMDEX-25 update drizzle config for drizzle studio

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,15 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import type { Config } from 'drizzle-kit';
 
 export default {
   schema: './lib/db-schemas',
   out: './drizzle',
+  driver: 'mysql2',
+  dbCredentials: {
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: 'evm-dex',
+  },
 } satisfies Config;
