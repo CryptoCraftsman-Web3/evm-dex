0e5d0e70f28617398fe2922bf5f3638842c9e2f2
CryptoCraftsman-Web3 <taylormoore97521@gmail.com>
2023-10-25 14:50:51 +0800

    EVMDEX-25 update environment types

--- environment.d.ts
+++ environment.d.ts
@@ -4,6 +4,9 @@ global {
       VERCEL_ENV: 'development' | 'preview' | 'production';
       NEXT_PUBLIC_WC_PID: string;
       NEXT_PUBLIC_INFURA_API_KEY: string;
+      DATABASE_HOST: string;
+      DATABASE_USERNAME: string;
+      DATABASE_PASSWORD: string;
     }
   }
 }
