8c76b1d3c452fcf7816b750579f72d1a14100e21
CryptoCraftsman-Web3 <taylormoore97521@gmail.com>
2023-10-27 14:37:02 +0800

    EVMDEX-29 disable react strictmode

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: false,
   webpack: (config) => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
     return config;
