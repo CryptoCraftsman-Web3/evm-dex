2f2d98ddd5c0baf734c2cdee28310b9d7495248a
CryptoCraftsman-Web3 <taylormoore97521@gmail.com>
2023-10-27 17:51:30 +0800

    EVMDEX-29 fix session check in connectkit auth

--- components/connectkit-auth.tsx
+++ components/connectkit-auth.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { signInWithSignature, signOut } from '@/lib/auth';
+import { getSession, signInWithSignature, signOut } from '@/lib/auth';
 import { getCookie } from 'cookies-next';
 import { sign } from 'crypto';
 import { useEffect } from 'react';
@@ -9,7 +9,7 @@ import { useAccount, useSignMessage } from 'wagmi';
 export default function ConnectKitAuth() {
   // wallet sign in hooks
   const { isConnected, isDisconnected, address } = useAccount();
-  const { signMessage, data: signature,  } = useSignMessage();
+  const { signMessage, data: signature } = useSignMessage();
 
   useEffect(() => {
     if (isDisconnected && !address) {
@@ -18,13 +18,12 @@ export default function ConnectKitAuth() {
   }, [isDisconnected, address]);
 
   useEffect(() => {
-    console.log(isConnected, address);
     if (isConnected && address) {
-      const authCookie = getCookie('serpent-swap-auth');
-      if (authCookie) return;
-
-      signMessage({
-        message: `I am signing into Serpent Swap with my wallet address: ${address}`,
+      getSession().then((session) => {
+        if (session?.address) return;
+        signMessage({
+          message: `I am signing into Serpent Swap with my wallet address: ${address}`,
+        });
       });
     }
   }, [isConnected, address]);
