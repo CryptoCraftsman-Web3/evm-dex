4082fb4a9a6c74776a93c5ca01cead44ea22e322
CryptoCraftsman-Web3 <taylormoore97521@gmail.com>
2023-10-27 17:55:01 +0800

    EVMDEX-25 Fetch user transactions in TransactionsPage server component

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,9 +1,30 @@
-import TransactionsClientPage from "./client-page";
+import { getSession } from '@/lib/auth';
+import TransactionsClientPage from './client-page';
+import { Transaction, transactions } from '@/lib/db-schemas/transaction';
+import { db } from '@/lib/database';
+import { desc, eq } from 'drizzle-orm';
 
 export default async function TransactionsPage() {
+  const session = await getSession();
+
+  let userTxs: Transaction[] = [];
+  if (session?.address) {
+    userTxs = await db.select()
+      .from(transactions)
+      .where(eq(transactions.from, session.address))
+      .orderBy(desc(transactions.blockNumber));
+  }
+
   return (
     <>
-      <TransactionsClientPage />
+      {session ? (
+        <TransactionsClientPage />
+      ) : (
+        <>
+          <h1>Transactions</h1>
+          <p>You are not signed in</p>
+        </>
+      )}
     </>
   );
-};
+}
