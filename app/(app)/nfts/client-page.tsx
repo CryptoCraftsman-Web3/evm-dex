b77e22434d208b7202f4028de57b129cb3fb2c66
CryptoCraftsman-Web3 <taylormoore97521@gmail.com>
2023-11-23 21:18:28 +0800

    EVMDEX-38 copy over nft path related code over to main from mark

---
+++ app/(app)/nfts/client-page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { AccountToken } from '@/types/common';
+
+type NFTsClientPageProps = {
+  nftBalances: AccountToken[];
+};
+
+export default function NFTsClientPage({ nftBalances }: NFTsClientPageProps) {
+  console.log(nftBalances);
+
+  return (
+    <div>
+      <h1>NFTs Client Page</h1>
+    </div>
+  );
+}
