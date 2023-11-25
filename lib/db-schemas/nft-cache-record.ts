267542e28bfa2479c123084d20ecdf69158f73f6
CryptoCraftsman-Web3 <taylormoore97521@gmail.com>
2023-11-25 15:14:09 +0800

    Add nftCacheRecord drizzle schema + migrations

---
+++ lib/db-schemas/nft-cache-record.ts
@@ -0,0 +1,22 @@
+import { datetime, int, mysqlTable, primaryKey, varchar } from 'drizzle-orm/mysql-core';
+
+export const nftCacheRecord = mysqlTable(
+  'nft_cache_record',
+  {
+    nftContractAddress: varchar('nft_contract_address', { length: 42 }).notNull(),
+    tokenId: int('token_id').notNull(),
+    ownerAddress: varchar('owner_address', { length: 42 }).notNull(),
+    tokenURI: varchar('token_uri', { length: 1024 }).notNull(),
+    lastUpdated: datetime('last_updated').notNull(),
+  },
+  (table) => {
+    return {
+      primaryKey: primaryKey({
+        columns: [table.nftContractAddress, table.tokenId, table.ownerAddress],
+      }),
+    };
+  }
+);
+
+export type NFTCacheRecord = typeof nftCacheRecord.$inferSelect;
+export type NewNFTCacheRecord = typeof nftCacheRecord.$inferInsert;
