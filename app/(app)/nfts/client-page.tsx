9bf9c8c985af043b90f1c54d8fcacdd7604ed657
CryptoCraftsman-Web3 <taylormoore97521@gmail.com>
2023-11-30 15:01:10 +0800

    EVMDEX-38 Add userNFTs prop

--- app/(app)/nfts/client-page.tsx
+++ app/(app)/nfts/client-page.tsx
@@ -1,15 +1,18 @@
 'use client';
 
+import { NFTCacheRecord } from '@/lib/db-schemas/nft-cache-record';
 import { AccountToken } from '@/types/common';
 
 type NFTsClientPageProps = {
+  userNFTs: NFTCacheRecord[];
 };
 
-export default function NFTsClientPage({  }: NFTsClientPageProps) {
+export default function NFTsClientPage({ userNFTs }: NFTsClientPageProps) {
+  console.log(userNFTs);
 
   return (
     <div>
-      <h1>NFTs Client Page</h1>
+      <h1>NFTs</h1>
     </div>
   );
 }
