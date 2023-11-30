e67fbaa13b9145f20a097f5d3f510b9893065d93
CryptoCraftsman-Web3 <taylormoore97521@gmail.com>
2023-11-30 15:39:33 +0800

    EVMDEX-38 enumerate user NFTs over NFTItem component

--- app/(app)/nfts/client-page.tsx
+++ app/(app)/nfts/client-page.tsx
@@ -1,7 +1,8 @@
 'use client';
 
+import NFTItem from '@/components/nfts/nft-item';
 import { NFTCacheRecord } from '@/lib/db-schemas/nft-cache-record';
-import { AccountToken } from '@/types/common';
+import { Grid, Paper, Stack, Typography } from '@mui/material';
 
 type NFTsClientPageProps = {
   userNFTs: NFTCacheRecord[];
@@ -11,8 +12,33 @@ export default function NFTsClientPage({ userNFTs }: NFTsClientPageProps) {
   console.log(userNFTs);
 
   return (
-    <div>
-      <h1>NFTs</h1>
-    </div>
+    <Stack spacing={4}>
+      <Typography variant="h4">
+        <b>NFTs</b>
+      </Typography>
+
+      <Paper
+        variant="outlined"
+        sx={{ p: 4, display: 'flex', flexDirection: 'column', gap: 4  }}
+      >
+        <Typography variant="h5">Your NFTs</Typography>
+        <Grid
+          container
+          spacing={3}
+        >
+          {userNFTs.map((nft) => (
+            <Grid
+              item
+              key={nft.tokenId}
+              xs={6}
+              md={3}
+              lg={2}
+            >
+              <NFTItem nftCacheRecord={nft} />
+            </Grid>
+          ))}
+        </Grid>
+      </Paper>
+    </Stack>
   );
 }
