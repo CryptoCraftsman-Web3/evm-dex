50ee944e318fe6b70b0a89a85cbb2b8f24ed1761
CryptoCraftsman-Web3 <taylormoore97521@gmail.com>
2023-12-01 15:04:34 +0800

    EVMDEX-38 add nft contracts data to fetched page data

--- app/app/nfts/page.tsx
+++ app/app/nfts/page.tsx
@@ -3,7 +3,8 @@ import { getSession } from '@/lib/auth';
 import NFTsClientPage from './client-page';
 import { db } from '@/lib/database';
 import { nftCacheRecord } from '@/lib/db-schemas/nft-cache-record';
-import { eq } from 'drizzle-orm';
+import { eq, inArray } from 'drizzle-orm';
+import { NFTContractCachedLog, nftContractCachedLog } from '@/lib/db-schemas/nft-contract-cached-log';
 
 export default async function NFTsPage() {
   const session = await getSession();
@@ -11,5 +12,26 @@ export default async function NFTsPage() {
 
   const userNFTs = await db.select().from(nftCacheRecord).where(eq(nftCacheRecord.ownerAddress, session.address));
 
-  return <NFTsClientPage userNFTs={userNFTs} />;
+  // get set of nft contract addresses
+  const nftContractAddresses = new Set<string>();
+  userNFTs.forEach((nft) => nftContractAddresses.add(nft.nftContractAddress));
+
+  // fetch nft contract cache log from db
+  const nftContracts = await db
+    .select()
+    .from(nftContractCachedLog)
+    .where(inArray(nftContractCachedLog.nftContractAddress, Array.from(nftContractAddresses)));
+
+  // map nft contract cache log to nft contract address
+  const nftContractsMap: { [key: string]: NFTContractCachedLog } = {};
+  nftContracts.forEach((nftContract) => {
+    nftContractsMap[nftContract.nftContractAddress] = nftContract;
+  });
+
+  return (
+    <NFTsClientPage
+      userNFTs={userNFTs}
+      nftContracts={nftContractsMap}
+    />
+  );
 }
