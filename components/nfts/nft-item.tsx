853d03811d691dd0a471f1da1fe2fa2479f1d75a
CryptoCraftsman-Web3 <taylormoore97521@gmail.com>
2023-12-01 15:09:42 +0800

    EVMDEX-38 4 changes to NFTItem component
    1. Added loading skeletons
    2. Added NFT contract info
    3. Added fractionalize button
    4. Added unknown nft fallback image

--- components/nfts/nft-item.tsx
+++ components/nfts/nft-item.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import { NFTCacheRecord } from '@/lib/db-schemas/nft-cache-record';
-import { useState, useEffect } from 'react';
-import { Avatar, Stack, Typography } from '@mui/material';
+import { NFTContractCachedLog } from '@/lib/db-schemas/nft-contract-cached-log';
+import { Skeleton, Stack, Typography, Button } from '@mui/material';
+import { useEffect, useState } from 'react';
 
 type NFTItemProps = {
   nftCacheRecord: NFTCacheRecord;
+  nftContract: NFTContractCachedLog;
 };
 
 type NFTMetadata = {
@@ -18,24 +20,32 @@ type NFTMetadata = {
   }[];
 };
 
-export default function NFTItem({ nftCacheRecord }: NFTItemProps) {
-  const [loading, setLoading] = useState<boolean>(false);
+export default function NFTItem({ nftCacheRecord, nftContract }: NFTItemProps) {
+  const [loading, setLoading] = useState<boolean>(true);
   const [metadata, setMetadata] = useState<NFTMetadata>();
   const [imageUrl, setImageUrl] = useState<string>();
 
   useEffect(() => {
     // load nft from metadata
     const { tokenURI } = nftCacheRecord;
-    if (!tokenURI) return;
+    if (!tokenURI) {
+      setLoading(false);
+      return;
+    }
 
     let metadataUri = tokenURI;
     if (tokenURI.startsWith('ipfs://')) {
       metadataUri = tokenURI.replace('ipfs://', 'https://cloudflare-ipfs.com/ipfs/');
     }
-    setLoading(true);
-    fetch(metadataUri)
-      .then((res) => res.json())
-      .then((metadata) => {
+
+    const fetchMetadata = async (uri: string) => {
+      console.log('fetching metadata', uri);
+      setLoading(true);
+      try {
+        const res = await fetch(uri);
+        if (!res.ok) throw new Error('Failed to fetch metadata');
+
+        const metadata = await res.json();
         setMetadata(metadata);
 
         // process image url
@@ -44,32 +54,117 @@ export default function NFTItem({ nftCacheRecord }: NFTItemProps) {
           imageUrl = metadata.image.replace('ipfs://', 'https://cloudflare-ipfs.com/ipfs/');
         }
         setImageUrl(imageUrl);
-      })
-      .catch((err) => {
+      } catch (err) {
         console.error(err);
-      })
-      .finally(() => {
+      } finally {
         setLoading(false);
-      });
+      }
+    };
+
+    fetchMetadata(metadataUri);
   }, []);
 
   return (
-    <Stack direction="column" >
-      <Avatar
-        variant="rounded"
-        src={imageUrl}
-        sx={{
-          width: '100%',
-          height: '100%',
-          aspectRatio: '1 / 1',
-          objectFit: 'cover',
-        }}
-      />
+    <Stack
+      direction="column"
+      spacing={1}
+      alignItems="center"
+      justifyContent="center"
+      sx={{
+        padding: '10px',
+      }}
+    >
+      {loading ? (
+        <>
+          <Skeleton
+            variant="rounded"
+            width="100%"
+            height="160px"
+          />
 
-      <Typography variant="body1" sx={{ textAlign: 'center' }}>
-        {metadata?.name || 'Unknown NFT'}
-      </Typography>
+          <Stack
+            direction="column"
+            spacing={0}
+            alignItems="center"
+            justifyContent="center"
+            width="100%"
+          >
+            <Skeleton
+              variant="text"
+              width="80%"
+            />
 
+            <Skeleton
+              variant="text"
+              width="60%"
+            />
+
+            <Skeleton
+              variant="text"
+              width="70%"
+            />
+          </Stack>
+
+          <Skeleton
+            variant="rounded"
+            width="100%"
+            height="40px"
+          />
+        </>
+      ) : (
+        <>
+          <img
+            src={imageUrl || '/images/unknown-nft.webp'}
+            style={{
+              width: '100%',
+              height: '100%',
+              aspectRatio: '1 / 1',
+              objectFit: 'cover',
+              padding: '10px',
+              borderRadius: '8px',
+            }}
+            onError={(e) => {
+              setImageUrl('/images/unknown-nft.webp');
+            }}
+          />
+
+          <Stack
+            direction="column"
+            spacing={0}
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Typography
+              variant="body1"
+              sx={{ textAlign: 'center' }}
+            >
+              <b>{metadata?.name || 'Unknown NFT'}</b>
+            </Typography>
+
+            <Typography
+              variant="body2"
+              sx={{ textAlign: 'center' }}
+            >
+              {nftContract?.nftContractName || 'Unknown Contract'}
+            </Typography>
+
+            <Typography
+              variant="body2"
+              sx={{ textAlign: 'center' }}
+            >
+              {nftContract?.nftContractSymbol || 'Unknown Symbol'} #{nftCacheRecord.tokenId}
+            </Typography>
+          </Stack>
+
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{ width: '100%' }}
+          >
+            Fractionalize
+          </Button>
+        </>
+      )}
     </Stack>
   );
 }
