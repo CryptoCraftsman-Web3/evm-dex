15d9e1d52a75f4da076b715e4f741146ebfb415d
CryptoCraftsman-Web3 <taylormoore97521@gmail.com>
2023-12-04 20:56:05 +0800

    EVMDEX-42 refactored NFTMetadata type to common types

--- types/common.ts
+++ types/common.ts
@@ -54,3 +54,13 @@ export interface TokenBalance {
   address: string;
   value: string;
 }
+
+export type NFTMetadata = {
+  name: string;
+  description: string;
+  image: string;
+  attributes?: {
+    trait_type: string;
+    value: string;
+  }[];
+};
