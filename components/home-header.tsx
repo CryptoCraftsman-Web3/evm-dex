d137ad1597b8ddb59024de27298f0c058c900a1b
CryptoCraftsman-Web3 <taylormoore97521@gmail.com>
2023-12-10 14:53:07 +0800

    Changed launch app href to /app/swap

--- components/home-header.tsx
+++ components/home-header.tsx
@@ -78,7 +78,7 @@ const AppHeader = () => {
             textAlign: 'right',
           }}
         >
-          <Link href={`https://app.${process.env.VERCEL_URL}`}>
+          <Link href="/app/swap">
             <Button variant='contained' color='primary' size='small'>Launch App</Button>
           </Link>
         </Grid>
@@ -113,7 +113,7 @@ const AppHeader = () => {
           spacing={2}
           justifyContent="flex-end"
         >
-          <Link href={'/pools'}>
+          <Link href="/app/swap">
             <Button variant='contained' color='primary' size='small'>Launch App</Button>
           </Link>
         </Stack>
