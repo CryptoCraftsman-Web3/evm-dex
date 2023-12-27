e1095def3d0a5bd0724089c1957ac546167bffbe
CryptoCraftsman-Web3 <taylormoore97521@gmail.com>
2023-12-27 20:19:11 +0800

    EVMDEX-45 Move server actions module to /lib/actions/tokens.ts

--- lib/actions/tokens.ts
+++ lib/actions/tokens.ts
@@ -1,7 +1,8 @@
 'use server';
 
-import { db } from '@/lib/database';
-import { erc20Token } from '@/lib/db-schemas/erc20-token';
+import { db } from '../database';
+import { erc20Token } from '../db-schemas/erc20-token';
+
 
 export default async function saveErc20Token(contractAddress: string, name: string, symbol: string, decimals: number) {
   await db
